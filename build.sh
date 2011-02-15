56c2fb182e5daaf0f8483e57d1bd90494f1a66f0
Dereckson <dereckson@espace-win.org>
2011-02-15 02:10:30 +0100

    Fixing linuxisms

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-if [ `uname -s` == "OpenBSD" -o `uname -s` == "FreeBSD" ]; then
+if [ "$(uname -s)" = "OpenBSD" -o "$(uname -s)" = "FreeBSD" ]; then
 	echo "Detected *BSD"
 	tar="gtar"
-elif [ `uname -s` == "SunOS" ]; then
+elif [ "$(uname -s)" = "SunOS" ]; then
 	echo "Detected SunOS"
 	tar="gtar"
 	CFLAGS="$CFLAGS -DSOLARIS -m64"
--- clean.sh
+++ clean.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo Cleaning up...
-rm -r out obj debian/data tmp
+rm -rf out obj debian/data tmp
