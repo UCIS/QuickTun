1ddacfa3bb24bdb5ae286d9886865f3a8bf8d35c
Ivo Smits <Ivo@UCIS.nl>
2011-02-27 22:47:22 +0100

    Include single protocol raw, nacl0 and nacltai binaries in debian package

--- debian/build.sh
+++ debian/build.sh
@@ -6,6 +6,9 @@ cp -r static data
 mkdir -p data/usr data/usr/sbin data/DEBIAN
 sed "s/%ARCHITECTURE%/${ARCH}/" -i data/DEBIAN/control
 sed "s/%VERSION%/${VERSION}/" -i data/DEBIAN/control
+cp ../out/quicktun.raw data/usr/sbin/
+cp ../out/quicktun.nacl0 data/usr/sbin/
+cp ../out/quicktun.nacltai data/usr/sbin/
 cp ../out/quicktun.debian data/usr/sbin/
 cp ../out/quicktun.keypair data/usr/sbin/
 fakeroot dpkg-deb --build data quicktun-${VERSION}_${ARCH}.deb
